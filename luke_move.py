b3dff74f02fe5874fbb9352b41c6d89cc281068a
Ian Loefgren <ian.loefgren@colorado.edu>
2016-06-03 16:42:41 -0600

    deckard still won't move from start, only after first goal point

--- luke_move.py
+++ luke_move.py
@@ -16,7 +16,7 @@ from pose import Pose
 """
 The goalHandler class publishes goal poses to the navigation stack via *robot_name*/move_base_simple/goal.
 
-An instance of goalHandler initilizes a ROS node that published to /move_base_simple/goal, subscribes to
+An instance of goalHandler initilizes a ROS node that published to *robot_name*/move_base_simple/goal, subscribes to
 *robot_name*/move_base/status for callbacks, and listens to tf transforms using an instance of the "pose" class.
 
 The goal poses come from discretePolicyTranslator.py, which takes the current xy position and returns the desired
@@ -37,16 +37,25 @@ class goalHandler(object):
 
 	def __init__(self, filename):
 		rospy.init_node('goal_sender')
+		self.dpt = discretePolicyTranslator(filename)
 		self.pose = Pose('',[0,0,0],'tf',None)
-		self.current_status = None
+		self.current_status = 3
 		self.tf_exist = False
 		self.current_position = None
-		self.goal_point = None
 		self.tf_exception_wrapper()
+		#if filename == "fakealphas1.txt":
+		#	self.current_position = [0,0,0,0]
+		#	self.goal_point = [0,0,0,0]
+		#elif filename == "fakealphas2.txt":		
+		#	self.current_position = [1,2,0,0]
+		#	self.goal_point = [1,2,0,0]
+		self.current_position = self.get_new_goal(self.current_position)
+		self.goal_point = self.get_new_goal(self.current_position)
+		#self.goal_point = self.get_new_goal(self.current_position)
 		self.pub = rospy.Publisher('/deckard/move_base_simple/goal',PoseStamped,queue_size=10)
+		self.send_goal()
 		rospy.Subscriber('/deckard/move_base/status',GoalStatusArray,self.callback)
-		self.dpt = discretePolicyTranslator(filename)
-		print("initial position: " + str(self.pose._pose))
+		print("initial position: " + str(self.current_position))
 
 	def tf_exception_wrapper(self):
 		tries = 0
@@ -55,9 +64,10 @@ class goalHandler(object):
 				self.pose.tf_update()	
 				self.tf_exist = True
 				self.current_position = self.pose._pose
-				self.goal_point = self.pose._pose
+				#self.goal_point = self.pose._pose
 			except tf.LookupException as error:
 				tries = tries + 1
+				self.tf_exist = False
 				print("\nError!")
 				print(error)
 				print("Waiting for transforms to become available. Will retry 10 times.") 
@@ -66,6 +76,7 @@ class goalHandler(object):
 
 	def callback(self,msg):
 		self.pose.tf_update()
+		#self.tf_exception_wrapper()
 		self.current_position = self.pose._pose
 		self.is_at_goal()		
 		print("status: " + str(self.current_status) + "\tcheck: " + str(self.current_status==3) + "\tcurrent position: " + str(self.current_position))
@@ -75,8 +86,15 @@ class goalHandler(object):
 		rospy.sleep(1)	
 
 	def is_at_goal(self):
-		if abs(self.goal_point[0] - self.current_position[0]) < 0.25 and abs(self.goal_point[1] - self.current_position[1]) < 0.25:
-			self.current_status = 3
+		tol = 0.25
+		print("Checking if arrived at goal")
+		try:
+			print("X goal diff: " + str(abs(self.goal_point[0] - self.current_position[0])) + "\tY goal diff: " + str(abs(self.goal_point[1] - self.current_position[1])))
+			if abs(self.goal_point[0] - self.current_position[0]) < tol and abs(self.goal_point[1] - self.current_position[1]) < tol:
+				self.current_status = 3
+		except TypeError:
+			print("Goal pose does not yet exist!")
+			self.current_status = 3		
 
 	def get_new_goal(self,current_position):
 		point = current_position
